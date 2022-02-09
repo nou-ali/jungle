06e2f7e8f5fba4775416c2e9fdac4cb43e927935
vagrant <noura4403@gmail.com>
2022-02-09 16:11:36 +0000

    final edits on login route

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,11 @@
 class SessionsController < ApplicationController
 
   def new
-    @user = User.new
     end
   
     def create
       if user = User.authenticate_with_credentials(params[:email], params[:password])
-        session[:user_id] = @user.id
+        session[:user_id] = user.id
         redirect_to '/'
       else
         redirect_to '/login'
